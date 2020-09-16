285794e62656f707d8781a662dd39fb2c3909f11
Tenji <patricepetit@hotmail.com>
2020-09-16 08:06:43 +0200

    feature/list: add peek

--- list/src/second.rs
+++ list/src/second.rs
@@ -2,7 +2,7 @@ use std::mem;
 
 #[derive(Debug)]
 pub struct List2<T> {
-    head: Link2<T>
+    head: Link2<T>,
 }
 
 type Link2<T> = Option<Box<Node2<T>>>;
@@ -14,31 +14,33 @@ struct Node2<T> {
 }
 
 impl<T> List2<T> {
-  pub fn new() -> Self {
-      List2{head: None}
-  }
+    pub fn new() -> Self {
+        List2 { head: None }
+    }
 
-  pub fn push(&mut self, elem: T) {
-      let node : Node2<T> = Node2{
-          elem: elem,
-          next: self.head.take()
-      };
+    pub fn push(&mut self, elem: T) {
+        let node: Node2<T> = Node2 {
+            elem: elem,
+            next: self.head.take(),
+        };
 
-      self.head = Some(Box::new(node));
-  }
+        self.head = Some(Box::new(node));
+    }
 
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|boxed_node| {
+            self.head = boxed_node.next;
+            return boxed_node.elem;
+        })
+    }
 
-  pub fn pop(&mut self) -> Option<T> {
-      self.head.take().map(|boxed_node| {
-        self.head = boxed_node.next;
-        return boxed_node.elem
-      })
-  }
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|boxed_node| &boxed_node.elem)
+    }
 
-
-  pub fn peek(&self) -> Option<&T> {
-      
-  }
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.elem)
+    }
 }
 
 impl<T> Drop for List2<T> {
@@ -57,7 +59,7 @@ mod test {
 
     #[test]
     fn basics() {
-        let mut list = List2::new();
+        let mut list: List2<i32> = List2::new();
 
         assert_eq!(list.pop(), None);
 
@@ -73,8 +75,22 @@ mod test {
 
         assert_eq!(list.pop(), Some(5));
         assert_eq!(list.pop(), Some(4));
-        
+
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
     }
+
+    #[test]
+    fn peek() {
+        let mut list = List2::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+
+        list.push(1); list.push(2); list.push(3);
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+
+    }
+
 }
