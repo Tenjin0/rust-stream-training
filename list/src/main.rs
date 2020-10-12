1342dbbe18db4df181ad5e82f801999bbc96fa50
Patrice Petit <ppetit@wynd.eu>
2020-10-12 17:55:54 +0200

    feature/list: retry to do list

--- list/src/main.rs
+++ list/src/main.rs
@@ -1,13 +1,22 @@
-use list::exercismio::SimpleLinkedList;
+use list::zero::List;
 
 fn main() {
-   let mut s = SimpleLinkedList::new();
-   s.push(3);
-   s.push(2);
+
+   let mut s: List<i32> = List::new();
+   
+
+   println!("{:?}", s);
    s.push(1);
-   s.push(0);
    println!("{:?}", s);
-   s.push_last(4);
-   // let list: List<i32> = List::Cons(1, Box::new(List::Cons(2, Box::new(List::Nil))));
+   s.push_last(2);
    println!("{:?}", s);
+   // let mut s = SimpleLinkedList::new();
+   // s.push(3);
+   // s.push(2);
+   // s.push(1);
+   // s.push(0);
+   // println!("{:?}", s);
+   // s.push_last(4);
+   // // let list: List<i32> = List::Cons(1, Box::new(List::Cons(2, Box::new(List::Nil))));
+   // println!("{:?}", s);
 }
--- list/src/zero.rs
+++ list/src/zero.rs
@@ -1,78 +1,60 @@
-use std::mem;
 
 #[derive(Debug)]
-pub struct List0 {
-    head: Link0
+pub struct List<T> {
+    head: Link<T>,
 }
 
-type Link0 = Option<Box<Node0>>;
+type Link<T> = Option<Box<Node<T>>>;
 
 #[derive(Debug)]
-struct Node0 {
-    elem: i32,
-    next: Link0,
+struct Node<T> {
+    val: T,
+    next: Link<T>
 }
 
-impl List0 {
-  pub fn new() -> Self {
-      List0{head: None}
-  }
-
-  pub fn push(&mut self, elem: i32) {
-      let node : Node0 = Node0{
-          elem: elem,
-          next: self.head.take()
-      };
-
-      self.head = Some(Box::new(node));
-  }
-
-
-  pub fn pop(&mut self) -> Option<i32> {
-      match self.head.take() {
-          Some(boxed_node) => {
-            self.head = boxed_node.next;
-            return Some(boxed_node.elem)
-          },
-          None => None
-      }
-  }
-}
-
-impl Drop for List0 {
-    fn drop(&mut self) {
-        let mut cur_link = self.head.take();
-
-        while let Some(mut boxed_node) = cur_link {
-            cur_link = mem::replace(&mut boxed_node.next, None);
+impl <T> List<T> {
+    pub fn new() -> Self {
+        List{
+            head: None
         }
     }
-}
 
-#[cfg(test)]
-mod test {
-    use super::List0;
+    pub fn push(&mut self, val: T) {
+        let node : Node<T>  = Node{
+            val: val,
+            next: self.head.take()
+        };
 
-    #[test]
-    fn basics() {
-        let mut list = List0::new();
+        self.head = Some(Box::new(node));
+    }
 
-        assert_eq!(list.pop(), None);
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.val)
+    }
 
-        list.push(1);
-        list.push(2);
-        list.push(3);
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.val)
+    }
 
-        assert_eq!(list.pop(), Some(3));
-        assert_eq!(list.pop(), Some(2));
 
-        list.push(4);
-        list.push(5);
-
-        assert_eq!(list.pop(), Some(5));
-        assert_eq!(list.pop(), Some(4));
-        
-        assert_eq!(list.pop(), Some(1));
-        assert_eq!(list.pop(), None);
+    pub fn push_last(&mut self, val: T) {
+        if self.head.is_none() {
+            self.push(val);
+        }
+        else {
+            let mut node = self.head.as_mut().unwrap();
+            loop {
+                if node.next.is_some() {
+                    node = node.next.as_mut().unwrap();
+                } else {
+                    break
+                }
+            }
+            let new_node : Node<T> = Node{
+                val: val,
+                next:None
+            };
+            node.next = Some(Box::new(new_node));
+        }
     }
 }
